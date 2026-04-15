c983be9daf9497fad93cafe07227326907e43845
lucascarv <lucascarvalhio077@gmail.com>
2026-04-14 22:49:52 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import Header from "@/components/header"
+import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
@@ -27,6 +30,28 @@ const Home = () => {
             className="rounded-xl object-cover"
           />
         </div>
+
+        <Card className="mt-6 p-0">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit bg-[#8162FF] text-white">
+                Confirmado
+              </Badge>
+              <h3>Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p>Barbearia fsw</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Fevereiro</p>
+              <p className="text-2xl">05</p>
+              <p>20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
