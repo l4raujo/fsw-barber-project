33a652954bcf0b4c7cca6f3c3ed11ced5486bb24
lucascarv <lucascarvalhio077@gmail.com>
2026-04-23 00:04:10 -0300

    feat: add seachService-list component in principal page

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,22 @@ import { SearchIcon } from "lucide-react"
 import Image from "next/image"
 import { db } from "./_lib/prisma"
 import BarberShopItem from "@/components/barbershop-item"
+import SearchServiceList from "@/components/search-service-list"
 
 const Home = async () => {
   // chamar meu banco de dados
   const barbershop = await db.baberShop.findMany({})
-
+  const popularBarbershops = await db.baberShop.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  })
   return (
     <div>
+      {/* Header */}
       <Header />
       <div className="p-5">
+        {/* Texto */}
         <h2 className="text-xl font-bold">Olá, Lucas!</h2>
         <p>Segunda-feira, 13 de abril</p>
 
@@ -27,6 +34,10 @@ const Home = async () => {
           </Button>
         </div>
 
+        {/* Busca Rapida */}
+        <SearchServiceList />
+
+        {/* Imagem */}
         <div className="relative min-h-[225px] w-full overflow-hidden">
           <Image
             src="/banner-01.png"
@@ -58,13 +69,26 @@ const Home = async () => {
           </CardContent>
         </Card>
         <h2 className="mt-6 mb-6 text-xs text-gray-400 uppercase">
-          RECOMENDADOS
+          recomendados
         </h2>
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershop.map((barbershop) => (
             <BarberShopItem barbershop={barbershop} key={barbershop.id} />
           ))}
         </div>
+        <h2 className="mt-6 mb-6 text-xs text-gray-400 uppercase">populares</h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (
+            <BarberShopItem barbershop={barbershop} key={barbershop.id} />
+          ))}
+        </div>
+        <footer>
+          <Card className="mt-6">
+            <CardContent className="px-5 py-6 text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </CardContent>
+          </Card>
+        </footer>
       </div>
     </div>
   )
